6059f9f23564b7a53b13046d1832a58701b74b89
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2020-01-31 08:23:56 -0800

    initial app complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+ThemeData appTheme = new ThemeData(
+  primaryColor: const Color(0xFF03A9F4),
+  primaryColorDark: const Color(0xFF0288D1),
+  primaryColorLight: const Color(0xFFB3E5FC),
+  accentColor: const Color(0xFFFFC107),
+  dividerColor: const Color(0xFFBDBDBD),
+  canvasColor: const Color(0xFFfafafa),
+  textSelectionColor: const Color(0xFF757575),
+);
 void main() {
   _setTargetPlatformForDesktop();
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '8-Ball',
+      theme: appTheme,
       home: BallPage(),
     ),
   );
@@ -21,15 +35,17 @@ void _setTargetPlatformForDesktop() {
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '8-Ball',
-      theme: new ThemeData(
-        primaryColor: const Color(0xFF388E3C),
-        accentColor: const Color(0xFFFF4081),
-        canvasColor: const Color(0xFFfafafa),
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Theme.of(context).primaryColorDark,
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(color: Color(0xFFFFFFFF)),
+        ),
       ),
-      home: new Ball(),
+      body: new Container(
+        child: Ball(),
+      ),
     );
   }
 }
@@ -42,10 +58,28 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  void changeBallFace() {
+    answer = Random().nextInt(5) + 1;
+  }
+
+  int answer = 1;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: child,
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(20.0),
+      color: Color(0xFF03A9F4),
+      child: Expanded(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              changeBallFace();
+            });
+          },
+          child: Image.asset('images/ball$answer.png'),
+        ),
+      ),
     );
   }
 }
